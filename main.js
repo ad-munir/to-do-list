13c53dfca0c76acd932e201ea62dc30a561f6d78
=munir37 <=monirayad37@gmail.com>
2022-10-20 22:08:37 +0100

    adding img folder containing background img, styling the app, using sweet alert

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//import swal from 'sweetalert';
+
 const input = document.getElementById("input");
 const btnAdd = document.getElementById("add");
 const btnClear = document.getElementById("clear");
@@ -5,19 +7,70 @@ const list = document.querySelector('.list');
 
 btnAdd.onclick = function(){
     if(input.value.trim() === ""){
-        alert('Please enter a vlue');
+        //alert('Please enter a value');
+        //swal("","Please enter a value","warning",{button:"I understood"});
+        swal({
+            //title:"0000000",
+            text:"Please enter a value",
+            icon:"warning",
+            buttons:"I understood",
+            timer:"4000",
+            className:"sweet"
+        })
+        
+
         return;
     }  
-    list.innerHTML +=`<li>${input.value} <button onclick='remove(event)'>remove</button></li>`;
+    list.innerHTML +=`<li><span>${input.value} </span><button onclick='remove(event)'>&#10006;</button></li>`;
 
     input.value ='';
     input.focus();
 } 
 
 function remove(e){
-    e.target.parentElement.remove();
+    swal({
+        title: "Are you sure?",
+        text: "Once removed, you will not be able to recover it!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+        className:"sweet"
+    })
+    .then((willDelete) => {
+        if (willDelete) {
+            e.target.parentElement.remove();
+            swal("item has been removed!","","success", {className:"sweet",timer:"3000"});
+        }
+    });
 }
 
 btnClear.onclick = function(){
-    list.innerHTML = '';
+    if(list.firstChild === null){
+        swal({
+            title:"Your to do list is empty",
+            text:"",
+            icon:"info",
+            buttons:"I understood",
+            timer:"4000",
+            className:"sweet"
+        })
+    }else{
+        swal({
+            title: "Are you sure?",
+            text: "Once you click OK, you will not be able to recover it!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+            className:"sweet"
+        })
+        .then((willDelete) => {
+            if (willDelete) {
+                list.innerHTML = '';
+                swal("To do list has been cleared!","","success", {className:"sweet",timer:"3000"});
+            }
+        });
+    }
+    
 }
+
+
